b141f1150ca80f27d76ae76e9504cffe9905f9d1
Bilal K. Hamada <bilalkamal2009@gmail.com>
2022-05-17 14:15:47 +0200

    project 4

---
+++ project4/network/models.py
@@ -0,0 +1,75 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "is_superuser": self.is_superuser,
+            "is_staff": self.is_staff,
+            "is_active": self.is_active,
+            "date_joined": self.date_joined,
+            "last_login": self.last_login,
+        }
+
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:20]
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "author": self.author.username
+        }
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='liker')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='liked_post')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.post.content[:20]}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user.username,
+            "post": self.post.serialize(),
+            "date": self.date
+        }
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='follower')
+    followed_user = models.ForeignKey(
+        User, related_name='followed_user', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.follower.username} follows {self.followed_user.username}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "follower": self.follower.username,
+            "followed_user": self.followed_user.username,
+            "date": self.date
+        }
---
+++ project4/network/views.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User
+
+
+def index(request):
+    # if the user is not logged in redirect them to the login page
+    return render(request, "network/index.html") if request.user.is_authenticated else HttpResponseRedirect(reverse("login"))
+
+
+def login_view(request):
+    # if the user is logged in automatically redirect them to the index page
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("index"))
+
+    if request.method != "POST":
+        return render(request, "network/login.html")
+    # Attempt to sign user in
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+
+    if user is None:
+        return render(request, "network/login.html", {
+            "message": "Invalid username and/or password."
+        })
+    login(request, user)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method != "POST":
+        return render(request, "network/register.html")
+    username = request.POST["username"]
+    email = request.POST["email"]
+
+    # Ensure password matches confirmation
+    password = request.POST["password"]
+    confirmation = request.POST["confirmation"]
+    if password != confirmation:
+        return render(request, "network/register.html", {
+            "message": "Passwords must match."
+        })
+
+    # Attempt to create new user
+    try:
+        user = User.objects.create_user(username, email, password)
+        user.save()
+    except IntegrityError:
+        return render(request, "network/register.html", {
+            "message": "Username already taken."
+        })
+    login(request, user)
+    return HttpResponseRedirect(reverse("index"))
